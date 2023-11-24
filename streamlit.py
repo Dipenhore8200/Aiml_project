30193b47949e2782af6251109f8a5e95f9c2f34d
Dipen hore <83294662+Dipenhore8200@users.noreply.github.com>
2023-11-24 14:51:44 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,19 @@
 import streamlit as st
 import pickle
 import numpy as np
+import os
+import requests
+from io import BytesIO
+
+# Function to download the model file from GitHub
+def download_model():
+    url = 'https://raw.githubusercontent.com/Dipenhore8200/Aiml_project/master/path-to-your-model/xgb.pkl'
+    response = requests.get(url)
+    model_bytes = BytesIO(response.content)
+    return pickle.load(model_bytes)
+
+# Load the pre-trained machine learning model
+xgboost = download_model()
 
 # Load the pre-trained machine learning model
 xgboost = pickle.load(open('xgb.pkl', 'rb'))
